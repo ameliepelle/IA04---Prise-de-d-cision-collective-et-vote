6de25f961bcde715063643769a9aef067613197c
ameliepelle <amelie.pelle2@gmail.com>
2022-11-01 18:58:36 +0100

    tentatives

--- cmd/vote.go
+++ cmd/vote.go
@@ -3,6 +3,8 @@ package main
 import (
 	procedures "TD3/comsoc"
 	"fmt"
+
+	"gitlab.utc.fr/lagruesy/ia04/utils"
 )
 
 func main() {
@@ -34,4 +36,26 @@ func main() {
 	fmt.Println("Ordre", alt2)
 	f := procedures.TieBreakFactory(alt2)
 	fmt.Println(f(alt3))
+
+	fmt.Println("permutations")
+	const n = 4
+
+	// création et affichage de la première permutation
+	perm := utils.FirstPermutation(n)
+	fmt.Println(perm)
+
+	// compteur des permutations
+	count := 1
+
+	// itération et affichage de la permutation suivante
+	perm, ok := utils.NextPermutation(perm)
+	for ok {
+		count++
+		fmt.Println(perm)
+		perm, ok = utils.NextPermutation(perm)
+	}
+
+	// on affiche la valeur de la factorielle...
+	fmt.Printf("\n%d!=%d\n", n, count)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module TD3
 
 go 1.19
+
+require gitlab.utc.fr/lagruesy/ia04 v0.2.0
